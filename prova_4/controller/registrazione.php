f4349b0bc4260e2069cf5beea3773ba074201db7
Alexinfotech <122553385+Alexinfotech@users.noreply.github.com>
2023-10-16 15:40:54 +0200

    Add files via upload

--- prova_4/controller/registrazione.php
+++ prova_4/controller/registrazione.php
@@ -5,12 +5,19 @@ $nome = $_POST['nome'];
 $cognome = $_POST['cognome'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$confermaPassword = $_POST['confermaPassword'];
+if ($password !== $confermaPassword) {
+    header("Location: ../public/index.php?error=passwordMismatch");
+    exit(); 
+}
 
 $conn = connessioneNAvigatore();
 
 $sql = "insert into utenti (nome, cognome, email, password) values ('$nome', '$cognome', '$email', '$password')";
 if (mysqli_query($conn, $sql)) {
     echo "Registrazione completata con successo!";
+    header("Refresh: 2; ../public/login.html");
+
 } else {
     echo "Errore: " . mysqli_error($conn);
 }
---
+++ prova_4/public/index.php
@@ -0,0 +1,34 @@
+<?php 
+if(isset($_GET['error']) && $_GET['error'] == 'passwordMismatch') {
+    echo "<p style='color:red;'>Le password inserite non coincidono!</p>";
+}
+?>
+<html>
+<body>
+<h1>Registrati</h1>
+<form action="../controller/registrazione.php" method="post">
+
+        <label for="nome">Nome</label>
+        <input type="text"  id="nome" name="nome" required>
+
+        <label for="cognome">Cognome</label>
+        <input type="text"  id="cognome" name="cognome" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email"  id="email" name="email" required>
+    
+   
+        <label for="password">Password:</label>
+        <input type="password"  id="password" name="password" required><br><br>
+
+        <label for="confermaPassword">Conferma Password:</label>
+        <input type="password" id="confermaPassword" name="confermaPassword" required><br><br>
+
+   
+        <input type="submit"  value="Registrati"><br><br>
+
+       <a href ="login.html">Loging</a>
+    </body>
+</html>
+
+
